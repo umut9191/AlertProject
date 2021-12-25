bd9bfce185c307f19eb6dcc694544c05125e0267
umut9191 <umutsurmeli91@gmail.com>
2021-12-25 16:36:11 +0300

    code writing completed

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var userNameTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var repeadPasswordTextField: UITextField!
     
@@ -21,14 +20,35 @@ class ViewController: UIViewController {
 
 
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Error", message: "username not valid", preferredStyle: UIAlertController.Style.alert)
+//        let alert = UIAlertController(title: "Error", message: "username not valid", preferredStyle: UIAlertController.Style.alert)
+//        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
+//            // actions when button pressing
+//            print("button clicked")
+//        }
+//        alert.addAction(okButton)
+//        self.present(alert, animated: true, completion: nil)
+        
+        if userNameTextField.text == "" {
+            alertFunc(title: "Error", message: "username can not be empty")
+        }else if passwordTextField.text == "" || repeadPasswordTextField.text == ""{
+            alertFunc(title: "Error", message: "password can not be empty")
+        }
+        else if passwordTextField.text != repeadPasswordTextField.text {
+          alertFunc(title: "Error", message: "password not match")
+        }
+        else {
+        alertFunc(title: "Success", message: "User OK")
+        }
+        
+    }
+    
+    func alertFunc(title: String , message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
-            // actions when button pressing
-            print("button clicked")
+            //print("button clicked")
         }
         alert.addAction(okButton)
         self.present(alert, animated: true, completion: nil)
-        
     }
 }
 
