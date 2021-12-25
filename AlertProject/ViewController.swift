e742839f7806f3758601ef3acd5a2b0836c2ffe3
umut9191 <umutsurmeli91@gmail.com>
2021-12-25 12:46:00 +0300

    screen designed and one alert example used in ViewController

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeadPasswordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Error", message: "username not valid", preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
+            // actions when button pressing
+            print("button clicked")
+        }
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
 
